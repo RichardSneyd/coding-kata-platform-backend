53caa026c9448d857df82b834d0b673ec05e7379
Richard Sneyd <richardsneyd@hotmail.com>
2022-11-08 10:26:28 +0000

    adds password reset routes and related logic; requires further testing

--- src/main/java/com/bnta/codecompiler/controllers/admin/UserAdminController.java
+++ src/main/java/com/bnta/codecompiler/controllers/admin/UserAdminController.java
@@ -1,17 +1,15 @@
 package com.bnta.codecompiler.controllers.admin;
 
+import com.bnta.codecompiler.models.users.PasswordResetInput;
 import com.bnta.codecompiler.models.users.User;
 import com.bnta.codecompiler.services.email.MailSenderService;
 import com.bnta.codecompiler.services.users.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
-import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -46,17 +44,24 @@ public class UserAdminController {
         try {
             var user = userService.findById(userId);
             var secret = encoder.encode(user.getUsername());
-
-            return ResponseEntity.ok("Thank you. A link as been sent to the registered email address for reset");
+            String formLink = "http://...";
+            mailService.sendEmail(user.getEmail(), "Password reset link", "Use this link to reset your password: " +
+            formLink + "?secret=" + secret);
+            return ResponseEntity.ok("Thank you. We've sent an email with reset instructions.");
         } catch (Exception e) {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 
-    @PostMapping("/password/reset/{userId}")
-    public ResponseEntity<?> resetPassword(@PathVariable Long userId, @RequestBody ) {
+    @PostMapping("/password/reset")
+    public ResponseEntity<?> resetPassword(@RequestBody PasswordResetInput pr) {
         try {
-            var user = userService.findById(userId);
+            var user = userService.findById(pr.getUserId());
+            if(!encoder.encode(user.getUsername()).equals(pr.getSecret())) {
+                throw new Exception("Wrong secret provided");
+            }
+            user.setPassword(encoder.encode(pr.getNewPassword()));
+            return ResponseEntity.ok().body("Password successfully updated");
 
         } catch (Exception e) {
             return ResponseEntity.notFound().build();
--- src/main/java/com/bnta/codecompiler/models/users/PasswordResetInput.java
+++ src/main/java/com/bnta/codecompiler/models/users/PasswordResetInput.java
@@ -1,17 +1,17 @@
 package com.bnta.codecompiler.models.users;
 
-public class PasswordRestDTO {
+public class PasswordResetInput {
     private Long userId;
     private String secret;
     private String newPassword;
 
-    public PasswordRestDTO(Long userId, String secret, String newPassword) {
+    public PasswordResetInput(Long userId, String secret, String newPassword) {
         this.userId = userId;
         this.secret = secret;
         this.newPassword = newPassword;
     }
 
-    public PasswordRestDTO() {
+    public PasswordResetInput() {
     }
 
     public Long getUserId() {
