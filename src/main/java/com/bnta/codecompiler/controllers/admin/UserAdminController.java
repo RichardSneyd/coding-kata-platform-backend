70853326705cad48cd29ba2d2db80c94554cd218
Richard Sneyd <richardsneyd@hotmail.com>
2022-11-08 10:01:52 +0000

    adds PasswordResetDTO

--- src/main/java/com/bnta/codecompiler/controllers/admin/UserAdminController.java
+++ src/main/java/com/bnta/codecompiler/controllers/admin/UserAdminController.java
@@ -1,10 +1,13 @@
 package com.bnta.codecompiler.controllers.admin;
 
 import com.bnta.codecompiler.models.users.User;
+import com.bnta.codecompiler.services.email.MailSenderService;
 import com.bnta.codecompiler.services.users.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.http.HttpResponse;
@@ -16,12 +19,18 @@ import java.util.Set;
 public class UserAdminController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private MailSenderService mailService;
+    @Autowired
+    private PasswordEncoder encoder;
 
     @GetMapping
     public Set<User> allUsers() {
         return userService.findAll();
     }
 
+    // allow password reset
+    //
     @PostMapping
     public ResponseEntity<User> add(@RequestBody User user) {
         try {
@@ -29,6 +38,28 @@ public class UserAdminController {
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(null, HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+
+    @GetMapping("/password/forgot/{userId}")
+    public ResponseEntity<?> forgotPassword(@PathVariable Long userId) {
+        try {
+            var user = userService.findById(userId);
+            var secret = encoder.encode(user.getUsername());
+
+            return ResponseEntity.ok("Thank you. A link as been sent to the registered email address for reset");
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/password/reset/{userId}")
+    public ResponseEntity<?> resetPassword(@PathVariable Long userId, @RequestBody ) {
+        try {
+            var user = userService.findById(userId);
+
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
         }
     }
 }
---
+++ src/main/java/com/bnta/codecompiler/models/users/PasswordRestDTO.java
@@ -0,0 +1,40 @@
+package com.bnta.codecompiler.models.users;
+
+public class PasswordRestDTO {
+    private Long userId;
+    private String secret;
+    private String newPassword;
+
+    public PasswordRestDTO(Long userId, String secret, String newPassword) {
+        this.userId = userId;
+        this.secret = secret;
+        this.newPassword = newPassword;
+    }
+
+    public PasswordRestDTO() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
