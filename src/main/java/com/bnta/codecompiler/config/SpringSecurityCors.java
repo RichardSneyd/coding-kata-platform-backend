ec9f0982da2a8958c55aa010feaaccaef1fa9797
Richard Sneyd <richardsneyd@hotmail.com>
2022-11-21 14:21:30 +0000

    renames GlobalSpringConfig to more appropriate WebMvcConfig; Extracts Spring Security CORS bean to SpringSecurityCors class

--- src/main/java/com/bnta/codecompiler/config/SpringSecurityCors.java
+++ src/main/java/com/bnta/codecompiler/config/SpringSecurityCors.java
@@ -1,20 +1,16 @@
 package com.bnta.codecompiler.config;
 
-import com.bnta.codecompiler.components.converters.StringToDifficultyConverter;
-import com.bnta.codecompiler.components.converters.StringToRoleConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
 @Configuration
-public class SpringGlobalConfig implements WebMvcConfigurer {
+public class SpringSecurityCors {
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -29,20 +25,4 @@ public class SpringGlobalConfig implements WebMvcConfigurer {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowedMethods("*");
-    }
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToDifficultyConverter());
-        registry.addConverter(new StringToRoleConverter());
-    }
-
-
 }
---
+++ src/main/java/com/bnta/codecompiler/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.bnta.codecompiler.config;
+
+import com.bnta.codecompiler.components.converters.StringToDifficultyConverter;
+import com.bnta.codecompiler.components.converters.StringToRoleConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToDifficultyConverter());
+        registry.addConverter(new StringToRoleConverter());
+    }
+
+}
