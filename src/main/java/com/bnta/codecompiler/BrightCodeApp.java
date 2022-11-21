dbe503d191cdda1587b5383db9f229414bfd8ab5
Richard Sneyd <richardsneyd@hotmail.com>
2022-11-21 14:17:35 +0000

    renamed main class to BrightCodeApp

--- src/main/java/com/bnta/codecompiler/BrightCodeApp.java
+++ src/main/java/com/bnta/codecompiler/BrightCodeApp.java
@@ -1,21 +1,17 @@
 package com.bnta.codecompiler;
 
-import com.bnta.codecompiler.services.email.MailSenderService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @ImportResource({"classpath*:application-context.xml"})
 @EnableAutoConfiguration
-public class CodeCompilerApplication {
+public class BrightCodeApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodeCompilerApplication.class, args);
+		SpringApplication.run(BrightCodeApp.class, args);
 	}
 
 }
