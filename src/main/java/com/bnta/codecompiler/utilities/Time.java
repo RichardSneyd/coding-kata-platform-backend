ebac1e6d0f7bfd8cae6142030b4ef72ce7819cbb
Richard Sneyd <richardsneyd@hotmail.com>
2022-12-13 14:15:57 +0000

    adds Time class to utilities, with setTimeout polyfill

---
+++ src/main/java/com/bnta/codecompiler/utilities/Time.java
@@ -0,0 +1,15 @@
+package com.bnta.codecompiler.utilities;
+
+public class Time {
+    public static void setTimeout(Runnable runnable, int delay){
+        new Thread(() -> {
+            try {
+                Thread.sleep(delay);
+                runnable.run();
+            }
+            catch (Exception e){
+                System.err.println(e);
+            }
+        }).start();
+    }
+}
