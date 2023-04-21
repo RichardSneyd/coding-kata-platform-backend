2cf2078fd0c17883091842d0269a212440a00369
Richard Sneyd <richardsneyd@hotmail.com>
2023-04-21 17:18:27 +0100

    using .env file to seperate configuration from code

--- src/main/java/com/bnta/codecompiler/config/GlobalSettings.java
+++ src/main/java/com/bnta/codecompiler/config/GlobalSettings.java
@@ -1,31 +1,64 @@
 package com.bnta.codecompiler.config;
 
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class GlobalSettings {
-    public final static boolean inProduction = false;
+//    public final static boolean inProduction = false;
+    private static final String CONFIG_FILE = ".env";
+    private static Properties properties = new Properties();
     private static String DOMAIN_NAME = "http://bntechacademy.com";
     private static String API_PORT = "8080";
-    private static String FRONTEND_PORT = "3000";
+    private static String FRONTEND_PORT = "80";
+
+    static {
+        try {
+            Path configFilePath = Paths.get(CONFIG_FILE);
+            if (Files.exists(configFilePath)) {
+                try (FileInputStream inputStream = new FileInputStream(CONFIG_FILE)) {
+                    properties.load(inputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("The specified config file does not exist.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static String getDomainName() {
-        return inProduction ? DOMAIN_NAME : "http://localhost";
+        return inProduction() ? DOMAIN_NAME : properties.getProperty("DOMAIN_NAME");
     }
 
-    public static String getApiOrigin() {
-        return getDomainName() + ":" + API_PORT;
-    }
-
-    public static String getFrontEndOrigin() {
-        return getDomainName() + ":" + FRONTEND_PORT;
-    }
-
-    public static String getApiPort() {
-        return API_PORT;
+    public static String getAPIPort() {
+        return inProduction() ? API_PORT : properties.getProperty("API_PORT");
     }
 
     public static String getFrontendPort() {
-        return FRONTEND_PORT;
+        return inProduction() ? FRONTEND_PORT : properties.getProperty("FRONTEND_PORT");
     }
+
+
+    public static boolean inProduction() {
+        return properties.getProperty("Environment") == null || !properties.getProperty("ENVIRONMENT").equals("development");
+    }
+
+
+    public static String getApiOrigin() {
+        return getDomainName() + ":" + getAPIPort();
+    }
+
+    public static String getFrontEndOrigin() {
+        return getDomainName() + ":" + getFrontendPort();
+    }
+
 
 
 }
