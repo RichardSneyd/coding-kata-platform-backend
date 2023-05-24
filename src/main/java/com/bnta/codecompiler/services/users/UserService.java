2de37d2f1fe738e43d041cb44f066df59220ad4a
Richard Sneyd <richardsneyd@hotmail.com>
2023-05-24 15:47:55 +0100

    improves the registration email format; it now mentions the username to use for login

--- src/main/java/com/bnta/codecompiler/services/users/UserService.java
+++ src/main/java/com/bnta/codecompiler/services/users/UserService.java
@@ -170,7 +170,7 @@ public class UserService {
     public void requestPasswordReset(User user) throws Exception {
         var secret = URLEncoder.encode(encoder.encode(user.getUsername()), StandardCharsets.UTF_8);
         String formLink = GlobalSettings.getFrontEndOrigin() + "/reset-password/" + user.getId();
-        mailService.sendEmail(user.getEmail(), "Password reset link", "Use this link to reset your password: " +
+        mailService.sendEmail(user.getEmail(), "Password reset link", "Welcome to BrightCode!\n\nYou're username is" + user.getUsername() + ".\n\nUse this link to reset your password: " +
                 formLink + "/" + secret);
     }
 
