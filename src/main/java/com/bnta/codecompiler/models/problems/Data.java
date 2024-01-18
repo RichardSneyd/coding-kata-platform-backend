a75a5bb63c40e39ba029a681e3ed6985471859b9
Richard Sneyd <richardsneyd@hotmail.com>
2024-01-18 09:27:29 +0000

    increase data max to 1000. VARCHAR will only use whats needed, up to this max

--- src/main/java/com/bnta/codecompiler/models/problems/Data.java
+++ src/main/java/com/bnta/codecompiler/models/problems/Data.java
@@ -11,7 +11,7 @@ public class Data {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
-    @Column
+    @Column(length = 1000)
     private String value;
 
     @Column
